985ec7156f4b1b3d76b1c6244cc66f3b5019f17f
Cody Raethke <craethke@amazon.com>
2021-09-01 20:34:40 -0500

    Add QR code scanning

--- src/database.js
+++ src/database.js
@@ -1,10 +1,14 @@
-import { LowSync, JSONFileSync } from 'lowdb';
+import { JSONFileSync, LowSync } from 'lowdb';
 
 const databaseFile = 'locationDatabase.json';
 const db = new LowSync(new JSONFileSync(databaseFile));
 
 db.read();
 db.data = db.data || { locations: [] };
+
+export const getLocations = () => {
+  return db.data.locations;
+};
 
 export const saveLocation = (location) => {
   db.data.locations.push(location);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import fs from 'fs';
+import https from 'https';
 import path, { dirname } from 'path';
-import * as db from './database.js';
 import { fileURLToPath } from 'url';
+import * as db from './database.js';
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -15,13 +18,25 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/locations', (req, res) => {
+    const locations = db.getLocations();
+    console.log('locations: ' + locations);
+    res.json(locations);
+});
+
 app.post('/location', (req, res) => {
     console.log(req.body);
     db.saveLocation(req.body);
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// });
 
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
 
+const httpsServer = https.createServer(options, app);
+httpsServer.listen(port);
